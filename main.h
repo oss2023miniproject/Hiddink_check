dfc8f214f7bac424a05078112a1a1f95b1575b3b
taeminkim-408 <82189651+taeminkim-408@users.noreply.github.com>
2023-05-07 17:12:06 +0900

    Create main.h

---
+++ main.h
@@ -0,0 +1,18 @@
+typedef struct{
+    char name [100];
+    int age;
+    double length;
+    double weight;
+    char detail [100];
+} Person;
+
+int loadData(Person **s);
+void saveData(Person **s, int count);
+int createProduct(Person *s);
+void readproduct(Person *s);
+void listproduct(Person *s[], int count);
+int updateProduct(Person *s);
+int selectdatano (Person *s[], int count);
+int deleteproduct(Person *s);
+void searchName(Person **s, int count);
+int selectMenu();
