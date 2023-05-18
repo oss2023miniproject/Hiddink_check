b955418066c96902add99bf966e7522f4fdfd120
taeminkim-408 <82189651+taeminkim-408@users.noreply.github.com>
2023-05-18 10:51:50 +0900

    Update main.h
    해당 함수들 구현을 위해서 변경이 되었습니다.

--- main.h
+++ main.h
@@ -1,9 +1,9 @@
 typedef struct{
-    char name [100];
-    int age;
-    double length;
-    double weight;
-    char detail [100];
+    char name [100];  //대표자 이름
+    int playground; //0일 경우 평봉, 1일 경우 히딩크
+    char teamname[100];  //팀이름
+    int countnumber;  //팀 전체 인원
+    int studentnumber;
 } Person;
 
 typedef struct {
@@ -11,16 +11,17 @@ typedef struct {
     int endtime;
     char teamname [100];
     char name[100];
+    int student_number;
 } Team;
 
-int loadData(Person **s,int selectnumber);
+int loadData(Person * *s,int selectnumber);
 void saveData(Person **s, int count,int selectnumber);
-int createProduct(Person *s);
+int createProduct(Person *s, int studentnumber);
 void readproduct(Person *s);
 void listproduct(Person *s[], int count);
-int updateProduct(Person *s);
+int updateProduct(Person *s, int studentnumber);
 int selectdatano (Person *s[], int count);
-int deleteproduct(Person *s);
+int deleteproduct(Person *s, int studentnumber);
 void searchName(Person **s, int count);
 int reserveTime(Team *t[]);
 void reserveteamName(Team *t[],int time);
@@ -28,4 +29,6 @@ void reserveSystem(Team **t);
 void teamfillmethod(Team **t);
 int selectMenu();
 int idcheck();
-
+void printeachTeam(Team *t);
+void printTeam(Team **t);
+void deleteTeam(Team **t,char a[]);
