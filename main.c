89288378c44079293d0e6d64ad43dbeeb701cbbe
taeminkim-408 <82189651+taeminkim-408@users.noreply.github.com>
2023-05-18 10:47:49 +0900

    main.c 함수 변경
    해당 함수에서 student id를 통해서 삭제하기 위해서 변경이 됬습니다.

--- main.c
+++ main.c
@@ -6,11 +6,17 @@
 
 int main(void){
     Person *s[20];
+    Team *a[24];
+    char y[100];//팀의 리더의 이름을 받는 변수,삭제시킬 때 필요.
+    int a_index=0;
+    int a_count=0;
+    int selectnumber;
     int count = 0, menu;
     int index =0;
-    count = loadData(s);
     int id = idcheck();
-    while (1){
+    teamfillmethod(a);
+    count = loadData(s,selectnumber);
+    while (1){//1->인원찾기(기능) 2->팀 예약 3->다른 팀 찾기
         menu = selectMenu();
         if (menu == 0) break;
         if (menu == 1){
@@ -18,7 +24,7 @@ int main(void){
         }
         else if (menu == 2){
             s[index]=(Person *)malloc(sizeof(Person));
-            count += createProduct(s[index++]);
+            count += createProduct(s[index++],id);
         }
         else if (menu == 3){
             int no = selectdatano(s,index);
@@ -26,7 +32,7 @@ int main(void){
                 printf("=> 취소됨!\n");
                 continue;
             }
-            updateProduct(s[no-1]);
+            updateProduct(s[no-1],id);
         }
         else if (menu == 4){
             int no = selectdatano(s,index);
@@ -38,14 +44,27 @@ int main(void){
             printf("정말로 삭제하시겠습까?(삭제 :1)");
             scanf("%d",&deletok);
             if(deletok ==1){
-                if(deleteproduct(s[no-1])) count--;
+                if(deleteproduct(s[no-1],id)) count--;
             }
         }
         else if (menu == 5){
-            saveData(s,index);
+            saveData(s,index,selectnumber);
         }
         else if (menu == 6){
             searchName(s,index);
+        }
+        else if (menu == 7){
+            printTeam(a);
+        }
+        else if (menu == 8){
+            reserveSystem(a);
+        }
+
+        else if(menu == 9){
+            printf("취소하실 팀의 리더의 이름을 입력해주세요! : ");
+            getchar();
+            scanf("%[^\n]s",y);
+            deleteTeam(a,y);
         }
     }
     printf("종료됨!\n");
