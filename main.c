8319924255c94fce9ed9512c7ff449380f8b8b3d
taeminkim-408 <82189651+taeminkim-408@users.noreply.github.com>
2023-05-21 16:55:29 +0900

    last check

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "main3.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -23,11 +23,9 @@ int main(void) {
 
   int id = idcheck(); //현재 이용자의 학번이다.
 
-  teamfillmethod(
-      a); //팀의 예약 구조체를 미리할당하는 것이다.(어차피 24시간 밖에 없으니!).
+  teamfillmethod(a); //팀의 예약 구조체를 미리할당하는 것이다.(어차피 24시간 밖에 없으니!).
 
-  printf(
-      "이용할 운동장을 고르세요: (평봉:1 히딩크:2) "); 
+  printf("이용할 운동장을 고르세요: (평봉:1 히딩크:2) "); 
   scanf("%d", &selectnumber);
   count = loadData(s, selectnumber);         //인원 파일
   loadTeamReserve(a, selectnumber);          //팀 예약 파일
@@ -81,20 +79,31 @@ int main(void) {
         menu = selectMenu3();
         scanf("%d",&selectnumber);
         if(selectnumber==1){
-          printeachTeam(a);
+          printTeam(a);
         }
         else if(selectnumber==2){
-          a[a_index]=(Person *)malloc(sizeof(Person));
-          reserveteamName(a,a_index);
+          //a[a_index]=(Team *)malloc(sizeof(Team));=>위에서 먼저 a에 동적메모리를 할당해서 다시 할 필요는 없을거 같습니다.
+          reserveSystem(a,id);
         }
         else if(selectnumber==3){
-         
+            updateTeam(a,id);
         }
+
         else if(selectnumber==4){
-         
+            int check;
+            getchar();
+            printf("대표 예약자 이름을 입력해주세요 : ");
+            scanf("%s",y);
+            printf("정말 삭제하시겠습니까?(YES: 1, No: 2) ");
+            scanf("%d",&check);
+            if(check == 1) deleteTeam(a,y,id);
+            else continue;
         }
         else if(selectnumber==5){
-          
+            getchar();
+            printf("찾고 싶은 팀의 대표 예약자 이름을 입력해주세요 : ");
+            scanf("%[^\n]s",y);
+            detectTeam(a,y);
         }
         else break;
       }
@@ -103,9 +112,12 @@ int main(void) {
       while(1){
         menu = selectMenu4();
         scanf("%d",&selectnumber);
-        if(selectnumber==1){
-          q[q_index]=(Search *)malloc(sizeof(Search));
-        q_index += inputSearchInformation(q,q_index,id); 
+        if(selectnumber==1){//여기서 포인터의  메모리 할당을 안함 이유는 만약에 학번이 같으면 여기서 하면 인덱스가 저절로 늘어나기 때문이다.
+            q_index = inputSearchInformation(q,q_index,id); 
+            //
+            
+            
+            printf("%d %d %s %s %s ",q_index,q[q_index]->student,q[q_index]->phonenumber,q[q_index]->teamname,q[q_index]->detail);
         }
         else if(selectnumber==2){
           int no = selectdatano3(q,q_index);
@@ -113,25 +125,24 @@ int main(void) {
                 printf("=> 취소됨!\n");
                 continue;
             }
-            updateInformation(&q[no-1],id);
+            updateInformation(q,no,id);
         }
         else if(selectnumber==3){
-          int no = selectdatano3(q,q_index);
+          /*int no = selectdatano3(q,q_index);
             if(no==0){
                 printf("=> 취소됨!\n");
                 continue;
-            }
-            int deletok;
-            printf("정말로 삭제하시겠습까?(삭제 :1)");
-            scanf("%d",&deletok);
-            if(deletok ==1){
-            }
+            }*/
+            deleteInformation(q,id,q_index);
         }
         else if(selectnumber==4){
           pritnInformation(q,q_index);
         }
         else if(selectnumber==5){
           saveSearchTeam(q,selectnumber,q_index);
+        }
+        else if(selectnumber == 6){
+            automatchingSystem(q,a,q_index,selectnumber);
         }
         else break;
       }
